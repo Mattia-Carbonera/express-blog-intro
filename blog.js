5e5648d8413d6d76496edda02cf1dc1cf03b48d7
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-19 16:47:01 +0100

    fix error

--- blog.js
+++ blog.js
@@ -2,6 +2,74 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const post = [
+  {
+    Title: "ciambellone",
+    Content: "Ricetta",
+    Image: "http://127.0.0.1:3000/ciambellone.jpeg",
+    Tags: [
+      "stringa1",
+      "stringa2",
+      "stringa3",
+      "stringa4",
+      "stringa5",
+      "stringa6",
+    ],
+  },
+  {
+    Title: "cracker barbabietola",
+    Content: "Ricetta",
+    Image: "http://127.0.0.1:3000/cracker_barbabietola.jpeg",
+    Tags: [
+      "stringa1",
+      "stringa2",
+      "stringa3",
+      "stringa4",
+      "stringa5",
+      "stringa6",
+    ],
+  },
+  {
+    Title: "pane fritto dolce",
+    Content: "Ricetta",
+    Image: "http://127.0.0.1:3000/pane_fritto_dolce.jpeg",
+    Tags: [
+      "stringa1",
+      "stringa2",
+      "stringa3",
+      "stringa4",
+      "stringa5",
+      "stringa6",
+    ],
+  },
+  {
+    Title: "pasta barbabietola",
+    Content: "Ricetta",
+    Image: "http://127.0.0.1:3000/pasta_barbabietola.jpeg",
+    Tags: [
+      "stringa1",
+      "stringa2",
+      "stringa3",
+      "stringa4",
+      "stringa5",
+      "stringa6",
+    ],
+  },
+  {
+    Title: "torta paesana",
+    Content: "Ricetta",
+    Image: "http://127.0.0.1:3000/torta_paesana.jpeg",
+    Tags: [
+      "stringa1",
+      "stringa2",
+      "stringa3",
+      "stringa4",
+      "stringa5",
+      "stringa6",
+    ],
+  },
+];
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -9,74 +77,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/bacheca", (req, res) => {
-  const post = [
-    {
-      Title: "ciambellone",
-      Content: "Ricetta",
-      Image: "http://127.0.0.1:3000/ciambellone.jpeg",
-      Tags: [
-        "stringa1",
-        "stringa2",
-        "stringa3",
-        "stringa4",
-        "stringa5",
-        "stringa6",
-      ],
-    },
-    {
-      Title: "cracker barbabietola",
-      Content: "Ricetta",
-      Image: "http://127.0.0.1:3000/cracker_barbabietola.jpeg",
-      Tags: [
-        "stringa1",
-        "stringa2",
-        "stringa3",
-        "stringa4",
-        "stringa5",
-        "stringa6",
-      ],
-    },
-    {
-      Title: "pane fritto dolce",
-      Content: "Ricetta",
-      Image: "http://127.0.0.1:3000/pane_fritto_dolce.jpeg",
-      Tags: [
-        "stringa1",
-        "stringa2",
-        "stringa3",
-        "stringa4",
-        "stringa5",
-        "stringa6",
-      ],
-    },
-    {
-      Title: "pasta barbabietola",
-      Content: "Ricetta",
-      Image: "http://127.0.0.1:3000/pasta_barbabietola.jpeg",
-      Tags: [
-        "stringa1",
-        "stringa2",
-        "stringa3",
-        "stringa4",
-        "stringa5",
-        "stringa6",
-      ],
-    },
-    {
-      Title: "torta paesana",
-      Content: "Ricetta",
-      Image: "http://127.0.0.1:3000/torta_paesana.jpeg",
-      Tags: [
-        "stringa1",
-        "stringa2",
-        "stringa3",
-        "stringa4",
-        "stringa5",
-        "stringa6",
-      ],
-    },
-  ];
-
   let totalPost = 0;
   post.forEach((post) => (totalPost += 1));
 
