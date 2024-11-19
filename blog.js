d4fd0ca9bb50a157885610550afab2b09e291277
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-19 15:38:25 +0100

    add rotta "/bacheca"

--- blog.js
+++ blog.js
@@ -9,3 +9,78 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Il server è online");
 });
+
+app.get("/bacheca", (req, res) => {
+  const post = [
+    {
+      Title: "titolo1",
+      Content: "contenuto1",
+      Image: "immagine1",
+      Tags: [
+        "stringa1",
+        "stringa2",
+        "stringa3",
+        "stringa4",
+        "stringa5",
+        "stringa6",
+      ],
+    },
+    {
+      Title: "titolo2",
+      Content: "contenuto2",
+      Image: "immagine2",
+      Tags: [
+        "stringa1",
+        "stringa2",
+        "stringa3",
+        "stringa4",
+        "stringa5",
+        "stringa6",
+      ],
+    },
+    {
+      Title: "titolo3",
+      Content: "contenuto3",
+      Image: "immagine3",
+      Tags: [
+        "stringa1",
+        "stringa2",
+        "stringa3",
+        "stringa4",
+        "stringa5",
+        "stringa6",
+      ],
+    },
+    {
+      Title: "titolo4",
+      Content: "contenuto4",
+      Image: "immagine4",
+      Tags: [
+        "stringa1",
+        "stringa2",
+        "stringa3",
+        "stringa4",
+        "stringa5",
+        "stringa6",
+      ],
+    },
+    {
+      Title: "titolo5",
+      Content: "contenuto5",
+      Image: "immagine5",
+      Tags: [
+        "stringa1",
+        "stringa2",
+        "stringa3",
+        "stringa4",
+        "stringa5",
+        "stringa6",
+      ],
+    },
+  ];
+
+  let totalPost = 0;
+  post.forEach((post) => (totalPost += 1));
+
+  res.json([post, "Total Post: " + totalPost]);
+});
